678d92ba14257ce16a73cd86fe2ddf7fea27cd12
Elaine <wojiadayima@yahoo.co.jp>
2026-09-13 15:19:53 +0900

    chore: カテゴリテーブルのスキーマを反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_09_08_042049) do
+ActiveRecord::Schema[7.2].define(version: 2026_09_13_061204) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "sort_order", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
