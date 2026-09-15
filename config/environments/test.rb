9ae543ff2d1ea31cce79aad6c6fd7c3b01ba755e
Elaine <wojiadayima@yahoo.co.jp>
2026-09-15 16:44:09 +0900

    feat: Bullet を導入してN+1クエリを検出

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # N+1 query detection (Bullet)
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.raise = true
+    Bullet.bullet_logger = true
+  end
 end
